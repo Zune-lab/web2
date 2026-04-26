14c08f26e0e3fb7a8f1ed4860d1d11a724491f13
Zune <nguyenhaivuong06@gmail.com>
2026-04-26 19:38:37 +0700

    Add files via upload

---
+++ singer/Parys/Parys.js
@@ -0,0 +1,45 @@
+export const Parys = {
+    name: "Parys",
+    type: "artist",
+    listeners: "18.466",
+
+    // Đường dẫn ảnh (nhớ tải ảnh thật và bỏ vào thư mục Parys nhé)
+    bannerImg: "singer/Parys/parys2.jpg", 
+    avatarImg: "singer/Parys/parys1.jpg",  
+    aboutImg: "singer/Parys/parys3.jpg",
+    
+    bio: "",
+    
+    albums: [
+        {
+            title: "Dear Min",
+            img: "singer/MIN/dear-min.jpg",
+            tracks: [
+                { title: "fragile (intro)", time: "0:35", videoSrc: "singer/MIN/sound/fragile-intro.mp4" },
+                { title: "Quá Muộn", time: "3:26", videoSrc: "singer/MIN/sound/qua-muon.mp4" },
+                { title: "LOSER (feat. Chillies)", time: "4:30", videoSrc: "singer/MIN/sound/loser-feat-chillies.mp4" },
+                { title: "phải viết bao nhiêu bản tình ca", time: "4:09", videoSrc: "singer/MIN/sound/phai-viet-bao-nhieu-ban-tinh-ca.mp4" },
+                { title: "Chế Độ Im Lặng", time: "3:05", videoSrc: "singer/MIN/sound/che-do-im-lang.mp4" },
+                { title: "Điều Em Khó Nói", time: "3:18", videoSrc: "singer/MIN/sound/dieu-em-kho-noi.mp4" },
+                { title: "ILYSMBIHTLYG (interlude)", time: "1:04", videoSrc: "singer/MIN/sound/ilysmbihtlyg-interlude.mp4" },
+                { title: "chẳng phải tình đầu sao đau đến thế (feat. Dangrangto, antransax)", time: "4:43", videoSrc: "singer/MIN/sound/chang-phai-tinh-dau-sao-dau-den-the.mp4" },
+                { title: "(từng là) Boyfriend, Girlfriend", time: "4:02", videoSrc: "singer/MIN/sound/boyfriend-girlfriend.mp4" },
+                { title: "Có Em Là Nhà", time: "4:10", videoSrc: "singer/MIN/sound/co-em-la-nha.mp4" }
+            ]
+        },
+        {
+            title: "50/50",
+            img: "singer/MIN/album_5050.jpg",
+            tracks: [
+                { title: "Late Blooming Flower", time: "0:47", videoSrc: "singer/MIN/sound/late-blooming-flower.mp3" },
+                { title: "Cà Phê", time: "3:10", videoSrc: "singer/MIN/sound/ca-phe.mp3" },
+                { title: "Trên Tình Bạn Dưới Tình Yêu", time: "3:19", videoSrc: "singer/MIN/sound/tren-tinh-ban-dua-tinh-yeu.mp3" },
+                { title: "Anh Qua Đây Đi", time: "3:20", videoSrc: "singer/MIN/sound/anh-qua-day-di.mp3" },
+                { title: "Phải Lòng Anh", time: "3:47", videoSrc: "singer/MIN/sound/phai-long-anh.mp3" },
+                { title: "Watching The Stars", time: "1:32", videoSrc: "singer/MIN/sound/watching-the-stars.mp3" },
+                { title: "Ngắm Sao (Dear MINiacs)", time: "3:23", videoSrc: "singer/MIN/sound/ngam-sao-dear-miniacs.mp3" },
+                { title: "Phải Lòng Anh (Dream Ver.)", time: "3:54", videoSrc: "singer/MIN/sound/phai-long-anh-dream-ver.mp3" }
+            ]
+        },
+    ]
+};
